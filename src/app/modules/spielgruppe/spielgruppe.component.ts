9374302ce4af791c09f7c7df866bf534f45e07cc
Linus <linus@wieland.lu>
2022-07-26 22:04:33 +0200

    reactive programming back icon

--- src/app/modules/spielgruppe/spielgruppe.component.ts
+++ src/app/modules/spielgruppe/spielgruppe.component.ts
@@ -1,14 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import { Location } from '@angular/common';
+import { NavigationEnd, Router } from '@angular/router';
+import {BehaviorSubject, EMPTY, filter, Observable, of, Subject, takeUntil} from 'rxjs';
 
 @Component({
   selector: 'app-spielgruppe',
   templateUrl: './spielgruppe.component.html',
   styleUrls: ['./spielgruppe.component.scss'],
 })
-export class SpielgruppeComponent {
-  constructor(private location: Location) {}
+export class SpielgruppeComponent implements OnInit, OnDestroy {
+  home: boolean = false;
+
+  private _ngDestroy$ = new Subject<void>();
+
+  constructor(private location: Location, private router: Router) {}
   back(): void {
-    this.location.back();
+    if (this.router.url === '/') {
+      this.location.back();
+    } else {
+      this.location.back();
+    }
+  }
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+      takeUntil(this._ngDestroy$),
+      filter((event) => event instanceof NavigationEnd))
+      .subscribe((x) => {
+        this.home = (x as NavigationEnd).url === '/';
+      });
+  }
+
+  ngOnDestroy() {
+    this._ngDestroy$.next();
+    this._ngDestroy$.complete();
   }
 }
