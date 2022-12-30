829b8cdb23fba8175a9e71448daf03111567151a
Linus <linus@wieland.lu>
2022-12-30 21:47:40 +0100

    settitle per page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {Meta, Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -8,13 +7,4 @@ import {Meta, Title} from "@angular/platform-browser";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-    constructor(private meta: Meta, private title: Title) {
-    this.meta.addTags([
-      {name: 'description', content:'Kind und Natur - die Indoor- und Outdoor-Spielgruppe in Luzern. Auf dem Tribschenhorn in Luzern. Für Kinder ab 3 bis 5 Jahre. Jeden Dienstag.'}
-    ])
-    this.setTitle('Kind und Natur - Home');
-  }
-  setTitle(newTitle: string){
-    this.title.setTitle(newTitle);
-  }
 }
--- src/app/modules/spielgruppe/spielgruppe.component.ts
+++ src/app/modules/spielgruppe/spielgruppe.component.ts
@@ -1,7 +1,16 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { NavigationEnd, Router } from '@angular/router';
-import {BehaviorSubject, EMPTY, filter, Observable, of, Subject, takeUntil} from 'rxjs';
+import {
+  BehaviorSubject,
+  EMPTY,
+  filter,
+  Observable,
+  of,
+  Subject,
+  takeUntil,
+} from 'rxjs';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-spielgruppe',
@@ -13,7 +22,12 @@ export class SpielgruppeComponent implements OnInit, OnDestroy {
 
   private _ngDestroy$ = new Subject<void>();
 
-  constructor(private location: Location, private router: Router) {}
+  constructor(
+    private location: Location,
+    private router: Router,
+    private meta: Meta,
+    private title: Title
+  ) {}
   back(): void {
     if (this.router.url === '/') {
       this.location.back();
@@ -26,12 +40,34 @@ export class SpielgruppeComponent implements OnInit, OnDestroy {
     this.home = this.router.url === '/';
     this.router.events
       .pipe(
-      takeUntil(this._ngDestroy$),
-      filter((event) => event instanceof NavigationEnd))
+        takeUntil(this._ngDestroy$),
+        filter((event) => event instanceof NavigationEnd)
+      )
       .subscribe((x) => {
-        console.log(x);
+        this.setDescription(
+          'Kind und Natur - die Indoor- und Outdoor-Spielgruppe in Luzern. Auf dem Tribschenhorn in Luzern. Für Kinder ab 3 bis 5 Jahre. Jeden Dienstag.',
+          (x as NavigationEnd).url
+        );
+        this.setTitle('Kind und Natur', (x as NavigationEnd).url);
+
         this.home = (x as NavigationEnd).url === '/';
       });
+  }
+
+  setTitle(newTitle: string, postfix: string) {
+    const postfixStripped = postfix == '/' ? 'Home' : postfix.replace('/', '');
+    this.title.setTitle(`${newTitle} - ${postfixStripped}`);
+  }
+
+  setDescription(newDescription: string, prefix: string) {
+
+    const prefixStripped = prefix == '/' ? 'Home' : prefix.replace('/', '');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${prefixStripped} : ${newDescription}`
+      },
+    ]);
   }
 
   ngOnDestroy() {
