53f13202ce1034fefed509543429b20a4ec746a3
Linus <linus@wieland.lu>
2023-04-15 19:42:26 +0200

    logo

--- src/app/modules/spielgruppe/spielgruppe.module.ts
+++ src/app/modules/spielgruppe/spielgruppe.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {CommonModule, NgOptimizedImage} from '@angular/common';
 
 import { SpielgruppeRoutingModule } from './spielgruppe-routing.module';
 import { SpielgruppeComponent } from './spielgruppe.component';
@@ -25,6 +25,6 @@ import {
     ContactFormComponent,
     SpielgruppeNewsletterComponent,
   ],
-  imports: [CommonModule, SpielgruppeRoutingModule, SharedModule],
+    imports: [CommonModule, SpielgruppeRoutingModule, SharedModule, NgOptimizedImage],
 })
 export class SpielgruppeModule {}
