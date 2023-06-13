bbaf269e96892d0ad796152312114fd83228151b
DonCorleone <linus@wieland.click>
2023-06-13 21:20:03 +0200

    confirmation mail

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,9 @@ export const environment = {
   SITE_ID: '',
   NODE_VERSION: '16',
   URL: '',
-  START_NEXT_SEMESTER: '2099-31-12'
+  START_NEXT_SEMESTER: '2099-31-12',
+  NETLIFY_EMAILS_SECRET: "undefined",
+  EMAIL_SENDER: "undefined",
 };
 
 /*
