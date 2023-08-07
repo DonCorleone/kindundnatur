84d692289581c7174d9b38570d60825be463c84b
DonCorleone <linus@wieland.click>
2023-08-07 09:30:21 +0200

    img style decentralize

--- src/app/modules/spielgruppe/spielgruppe-leitung/spielgruppe-leitung.component.ts
+++ src/app/modules/spielgruppe/spielgruppe-leitung/spielgruppe-leitung.component.ts
@@ -2,7 +2,16 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-spielgruppe-leitung',
+  styles: [
+    `
+      img {
+        width: 100%;
+        max-width: 300px;
+        filter: brightness(80%);
+      }
+    `,
+  ],
   templateUrl: './spielgruppe-leitung.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SpielgruppeLeitungComponent {}
