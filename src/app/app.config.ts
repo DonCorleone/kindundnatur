e1b6fed0c455d2db18030d57dc11d8ee26d88aff
linus <linus@wieland.click>
2023-11-12 16:50:06 +0100

    scrollToTop

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,28 @@
 import {ApplicationConfig, LOCALE_ID} from '@angular/core';
-import {provideRouter} from '@angular/router';
+import {
+  InMemoryScrollingFeature,
+  InMemoryScrollingOptions,
+  provideRouter,
+  withInMemoryScrolling
+} from '@angular/router';
 
 import {routes} from './app.routes';
-import {provideClientHydration} from '@angular/platform-browser';
+import {bootstrapApplication, provideClientHydration} from '@angular/platform-browser';
 import {provideHttpClient} from "@angular/common/http";
 import {DatePipe, registerLocaleData} from "@angular/common";
 import localeCh from '@angular/common/locales/de-CH';
 import Swiper from "swiper";
 
-
 registerLocaleData(localeCh);
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature =
+  withInMemoryScrolling(scrollConfig);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(), DatePipe, {
+  providers: [provideRouter(routes, inMemoryScrollingFeature), provideClientHydration(), provideHttpClient(), DatePipe, {
     provide: LOCALE_ID,
     useValue: 'de-CH'
   }, Swiper]
