d8084e9ce9988a5204dc6e444355f46c9bd58d18
linus <linus@wieland.click>
2023-11-12 20:54:57 +0100

    regex fix

--- src/app/components/swiper/swiper.component.ts
+++ src/app/components/swiper/swiper.component.ts
@@ -89,7 +89,7 @@ export class SwiperComponent implements OnInit, AfterViewInit {
       .subscribe((result) => {
         for (const query of Object.keys(result.breakpoints)) {
           if (result.breakpoints[query]) {
-            const match = query.match('\\(min-width:\\s(\\d+)\\.98px\\)');
+            const match = query.match('\\(min-width:\\s(\\d+)px\\)');
             const width = match?.length ? match[1] : '2048';
             const height = 3/4*+width;
             this.files$ = this.imageService.listAssets().pipe(
