8349e4104ebc0b2f8473b9a9ebbc68108166450c
linus <linus@wieland.click>
2023-11-12 21:38:20 +0100

    cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,4 @@
 import {Routes} from "@angular/router";
-import {SpielgruppeComponent} from "./components/spielgruppe/spielgruppe.component";
-import {SpielgruppeOverviewComponent} from "./components/spielgruppe-overview/spielgruppe-overview.component";
-import {SpielgruppeInfoComponent} from "./components/spielgruppe-info/spielgruppe-info.component";
-import {SpielgruppeLeitungComponent} from "./components/spielgruppe-leitung/spielgruppe-leitung.component";
-import {
-  SpielgruppeLeitungEvamariaComponent
-} from "./components/spielgruppe-leitung-evamaria/spielgruppe-leitung-evamaria.component";
-import {SpielgruppeOrtComponent} from "./components/spielgruppe-ort/spielgruppe-ort.component";
-import {SpielgruppeNewsletterComponent} from "./components/spielgruppe-newsletter/spielgruppe-newsletter.component";
-import {ContactFormComponent} from "./components/contact-form/contact-form.component";
-import {SuccessComponent} from "./components/success/success.component";
-
 export const routes: Routes = [
   {
     path: '',
