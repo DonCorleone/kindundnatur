e46fddc956b35a4017112fed1805e7e3b890f8e9
linus <linus@wieland.click>
2023-11-13 19:15:22 +0100

    cleanup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,36 +13,36 @@ export const routes: Routes = [
       {
         path: 'info',
         loadComponent: () =>
-          import("./components/spielgruppe-info/spielgruppe-info.component").then((c) => c.SpielgruppeInfoComponent) // another child route component that the router renders
+          import("./components/spielgruppe-info/spielgruppe-info.component").then((c) => c.SpielgruppeInfoComponent)
       },
       {
         path: 'leitung',
         loadComponent: () =>
-          import("./components/spielgruppe-leitung/spielgruppe-leitung.component").then((c) => c.SpielgruppeLeitungComponent) // another child route component that the router renders
+          import("./components/spielgruppe-leitung/spielgruppe-leitung.component").then((c) => c.SpielgruppeLeitungComponent)
       }, {
         path: 'leitungEvamaria',
         loadComponent: () =>
-          import("./components/spielgruppe-leitung-evamaria/spielgruppe-leitung-evamaria.component").then((c) => c.SpielgruppeLeitungEvamariaComponent) // another child route component that the router renders
+          import("./components/spielgruppe-leitung-evamaria/spielgruppe-leitung-evamaria.component").then((c) => c.SpielgruppeLeitungEvamariaComponent)
       },
       {
         path: 'ort',
         loadComponent: () =>
-          import("./components/spielgruppe-ort/spielgruppe-ort.component").then((c) => c.SpielgruppeOrtComponent) // another child route component that the router renders
+          import("./components/spielgruppe-ort/spielgruppe-ort.component").then((c) => c.SpielgruppeOrtComponent)
       },
       {
         path: 'newsletter',
         loadComponent: () =>
-          import("./components/spielgruppe-newsletter/spielgruppe-newsletter.component").then((c) => c.SpielgruppeNewsletterComponent), // another child route component that the router renders
+          import("./components/spielgruppe-newsletter/spielgruppe-newsletter.component").then((c) => c.SpielgruppeNewsletterComponent),
       },
       {
         path: 'contact',
         loadComponent: () =>
-          import("./components/contact-form/contact-form.component").then((c) => c.ContactFormComponent), // another child route component that the router renders
+          import("./components/contact-form/contact-form.component").then((c) => c.ContactFormComponent),
       },
       {
         path: 'success',
         loadComponent: () =>
-          import("./components/success/success.component").then((c) => c.SuccessComponent), // another child route component that the router renders
+          import("./components/success/success.component").then((c) => c.SuccessComponent),
       },
     ],
   },
